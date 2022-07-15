b09c86b43f257c81a627c3701735bf951bf75202
theUpsider <davidvfischer@gmail.com>
2022-07-15 12:16:07 +0200

    removed unnecessary code added KI says something different

--- frontend/src/components/Charts/CustomBarChart.js
+++ frontend/src/components/Charts/CustomBarChart.js
@@ -33,42 +33,15 @@ const data = [
       },
       {
             name: "Sequential / Global",
-            uv: 4000,
+            uv: -4000,
             pv: 3000,
             amt: 550
       }
 ];
-const YAxisLeftTick = ({ y, payload: { value } }) => {
-      return (
-            <Text x={0} y={y} textAnchor="start" verticalAnchor="middle" scaleToFit>
-                  {value}
-            </Text>
-      );
-};
-let ctx;
-export const measureText14HelveticaNeue = text => {
-      if (!ctx) {
-            ctx = document.createElement("canvas").getContext("2d");
-            ctx.font = "14px 'Helvetica Neue";
-      }
 
-      return ctx.measureText(text).width;
-};
-const BAR_AXIS_SPACE = 10;
 
 export default function CustomBarChart() {
-      const maxTextWidth = useMemo(
-            () =>
-                  data.reduce((acc, cur) => {
-                        const value = cur["name"];
-                        const width = measureText14HelveticaNeue(value.toLocaleString());
-                        if (width > acc) {
-                              return width;
-                        }
-                        return acc;
-                  }, 0),
-            []
-      );
+
       return (
             <ResponsiveContainer height={60 * data.length} debounce={50} style={{ margin: '20px' }}>
 
@@ -79,7 +52,6 @@ export default function CustomBarChart() {
                         barGap="0"
                         barSize={30}
                   >
-                        {/* <CartesianGrid strokeDasharray="3 3" /> */}
                         <XAxis hide type="number" />
                         <YAxis
                               orientation="left"
